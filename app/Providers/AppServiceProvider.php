ec47daf3853a96dfb3e97047de48c0bf5dec6094
arsusbravo <ario_susmaryono@hotmail.com>
2026-05-15 00:06:22 +0200

    change error db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Required for older MySQL/MariaDB with utf8mb4: limits indexed VARCHAR keys to 191 chars (764 bytes < 767 limit)
+        Schema::defaultStringLength(191);
+
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
